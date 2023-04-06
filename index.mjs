d83811dab95354a80299fc0ccd517b1e525ddad7
dangliming <danglm@qq.com>
2023-04-06 23:43:54 +0800

    feat: update cron

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ import Cron from 'cron';
 import downLoadBingWallpaper from './src/downLoadBingWallpaper.mjs'
 
 new Cron.CronJob(
-  '* * 9 * * *',
+  '0 0 9 * * *',
   downLoadBingWallpaper,
   null,
   true,
